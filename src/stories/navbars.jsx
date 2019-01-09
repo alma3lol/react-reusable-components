9f344923a397989ba248c114464685f47615b2d0
Omar Almalol <omar.alma3lol@gmail.com>
2019-01-10 01:43:15 +0200

    Added explanation to Navbars

--- src/stories/navbars.jsx
+++ src/stories/navbars.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { storiesOf } from '@storybook/react';
 import Navbars from '../navbars';
+import './navbars.scss';
 
 const { GreenHoverNavbar } = Navbars;
 
@@ -14,5 +15,14 @@ const navs = {
 
 storiesOf('Navbars/With Hover', module)
 .add('Green Hover', () => (
-    <GreenHoverNavbar navs={navs} />
+    <div className="container">
+        <GreenHoverNavbar navs={navs} />
+        <div className="explain">
+            <h3>Navbar with Green Hover</h3><br/>
+            <p>This component accepts one param: <code>navs</code>.</p><br/>
+            <p><code>navs</code> param is an object which contains pairs of <code>link</code>:<code>text</code>.</p>
+            <p>Here's an example of how <code>navs</code> is shaped:</p><br/>
+            <pre dangerouslySetInnerHTML={{ __html: "{\r\n   '/':           'Home',\r\n   '/services/':  'Services',\r\n   '/portfolio/': 'Portfolio',\r\n   '/about/':     'About',\r\n   '/contact/':   'Contact'\r\n}"}} />
+        </div>
+    </div>
 ));
